981836f92b98e3a9d5097ead290e19b80899b852
Behbud E. Zeynalov <behbudez@code.local>
2022-12-04 11:44:12 +0400

    init

---
+++ 112/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _112
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
